6f2eeac9fd43890a9fa2d7899067c42cb3250bdd
(s) Morgan Hill <morgan.hill@students.plymouth.ac.uk>
2019-10-16 11:22:56 +0100

    Stock tracker practical 3 step 6

--- Week03/StockTracker/src/stocktracker/stockdatamodel/StockItem.java
+++ Week03/StockTracker/src/stocktracker/stockdatamodel/StockItem.java
@@ -11,4 +11,50 @@ package stocktracker.stockdatamodel;
  */
 public class StockItem {
     
+    private String name = "UNKNOWN";
+    private Integer quantityInStock = 0;
+    private Double sellingPrice = 1000000.00;
+    private Double costPrice = 1000000.00;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if(name != null && !name.isEmpty()){
+        this.name = name;
+        }
+    }
+
+    public Integer getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public void setQuantityInStock(Integer quantityInStock) {
+        if(quantityInStock != null && quantityInStock >= 0){
+        this.quantityInStock = quantityInStock;
+        }
+    }
+    
+    public Double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Double sellingPrice) {
+        if(sellingPrice != null && sellingPrice >= this.costPrice && sellingPrice >= 0) {
+        this.sellingPrice = sellingPrice;
+        }
+    }
+
+    public Double getCostPrice() {
+        return costPrice;
+    }
+
+    public void setCostPrice(Double costPrice) {
+        if(costPrice != null && costPrice >= 0)
+        {   
+        this.costPrice = costPrice;
+        }
+    }
+    
 }
