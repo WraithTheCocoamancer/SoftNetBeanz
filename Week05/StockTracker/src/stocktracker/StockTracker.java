9b71fdf8af16bd037e244c7dd353bd266b3f4b85
(s) Morgan Hill <morgan.hill@students.plymouth.ac.uk>
2019-10-30 11:18:49 +0000

    added constructor

---
+++ Week05/StockTracker/src/stocktracker/StockTracker.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package stocktracker;
+
+import stocktracker.stockdatamodel.*;
+
+
+/**
+ *
+ * @author mhill9
+ */
+public class StockTracker {
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PhysicalStockItem objPhysicalItem =
+                new PhysicalStockItem("Snuff: A Diskworld book by Terry Pratchett", 100);
+        ServiceStockItem objVirtualItem =
+                new ServiceStockItem("Wintersmith: A Diskworld eBook by Terry Pratchett");
+        
+        String strMessage = objPhysicalItem.getName()
+                + ", Is in stock = " + objPhysicalItem.isInStock()
+                + ", Qty in stock: " + objPhysicalItem.getQuantityInStock();
+        System.out.println(strMessage);
+        
+        strMessage = objVirtualItem.getName()
+                + ", Is in stock = " + objVirtualItem.isInStock()
+                + ", Qty in stock: " + objVirtualItem.getQuantityInStock();
+    }
+    
+}
